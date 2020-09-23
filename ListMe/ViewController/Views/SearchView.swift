fede937e2f6e827e240dccba8972ca76551a7c6c
Javra Software <javra@Javras-MacBook-Air-3.local>
2020-09-23 20:53:56 +0545

    Add search icon in bar

--- ListMe/ViewController/Views/SearchView.swift
+++ ListMe/ViewController/Views/SearchView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SearchView: View {
     
-    @State var searchText = String()
+    @State private var searchText = String()
     
     let isModalClosed: (()-> Void)?
     
@@ -20,49 +20,47 @@ struct SearchView: View {
     ]
     
     var body: some View {
-        
         VStack {
+            
             HStack {
                 TextField("Enter Search Text", text: $searchText)
-                    .padding()
+                    .padding(.horizontal, 40)
                     .frame(width: UIScreen.main.bounds.width - 90, height: 40, alignment: .leading)
                     .background(Color(#colorLiteral(red: 0.9294475317, green: 0.9239223003, blue: 0.9336946607, alpha: 1)))
                     .clipped()
                     .cornerRadius(10)
-                
+                    .overlay(
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                .padding(.leading, 16)
+                        }
+                    )
                 Spacer()
-                
                 Button(action: {
                     isModalClosed?()
                 }, label: {
                     Text("Close")
                 })
                 .font(.title3)
-            }
-            
-            .padding()
+            }.padding()
             
             ScrollView(showsIndicators: false) {
                 LazyVGrid(columns: layout, spacing: 21) {
-                    
                     ForEach(data, id: \.self) { item in
                         VStack {
                             RoundedRectangle(cornerRadius: 10, style: .continuous)
                                 .fill(Color.blue)
                                 .frame(height: 95)
                                 .padding(.horizontal)
-                                .shadow(radius: 20)
-                            
-                            Text(item)
-                                .foregroundColor(.secondary)
-                            
+                                .shadow(color: Color.black.opacity(0.5), radius: 5, x: 0, y: 2)
+                            Text(item).foregroundColor(.secondary)
                         }
                     }
                 }
             }
         }
-        
-        
     }
 }
 
